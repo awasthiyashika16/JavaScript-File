50cb1a4762974116df1cf8f1e5b8e95a766c3d17
awasthiyashika16 <awasthiyashika78@gmail.com>
2026-09-15 14:44:13 +0000

    container config done

---
+++ .devcontainer/Basics/variables.js
@@ -0,0 +1,8 @@
+const accountId=238946;
+let accountEmail="yashi@gmail.com";
+var accountPassword="238946";
+accountCity="Mumbai";
+let accountState;
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
